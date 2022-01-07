169bf0fbc0a90f8c151bbaebcc0cd1893a4c3681
codriv <combodriver4@yahoo.at>
2022-01-07 18:40:19 +0100

    week11-day05-labs-task: Lottery

--- week11-lab-tasks/src/main/java/day05/Lottery.java
+++ week11-lab-tasks/src/main/java/day05/Lottery.java
@@ -8,6 +8,7 @@ public class Lottery {
 
     private int numbers;
     private int selected;
+    private Random random = new Random();
 
     public Lottery(int numbers, int selected) {
         this.numbers = numbers;
@@ -15,15 +16,25 @@ public class Lottery {
     }
 
     public List<Integer>  startLottery() {
-        Random random = new Random();
         List<Integer> winner = new ArrayList<>();
         int win;
         for (int i = 0; i < selected; i++) {
-            win = random.nextInt(1, numbers);
+            win = random.nextInt(1, numbers + 1 );
             while (winner.contains(win)) {
-                win = random.nextInt(1, numbers);
+                win = random.nextInt(1, numbers + 1);
             }
             winner.add(win);
+        }
+        return winner;
+    }
+
+    public List<Integer>  startLottery2() {
+        List<Integer> winner = new ArrayList<>();
+        while (winner.size() < selected) {
+            int win = random.nextInt(1, numbers + 1);
+            if (!winner.contains(win)) {
+                winner.add(win);
+            }
         }
         return winner;
     }
--- week11-lab-tasks/src/test/java/day05/LotteryTest.java
+++ week11-lab-tasks/src/test/java/day05/LotteryTest.java
@@ -3,6 +3,7 @@ package day05;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,8 +13,8 @@ class LotteryTest {
 
     @Test
     void startLottery() {
-        System.out.println(lottery.startLottery());
-        List<Integer> wins = lottery.startLottery();
+        System.out.println(lottery.startLottery2());
+        List<Integer> wins = lottery.startLottery2();
         for (int i = 0; i < wins.size(); i++) {
             for (int j = i + 1; j < wins.size(); j++) {
                 if (wins.get(i) == wins.get(j)) {
@@ -22,5 +23,11 @@ class LotteryTest {
             }
         }
         System.out.println("Jó!");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 30; i++) {
+            System.out.print((new Random()).nextInt(3,6));
+        }
     }
 }
