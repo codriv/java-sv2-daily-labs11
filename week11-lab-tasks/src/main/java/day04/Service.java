2fcbd67f37ad8350ddda457184623e38c79cf166
codriv <combodriver4@yahoo.at>
2022-01-07 07:00:35 +0100

    week11-lab-tasks - Day04 - Webshop

---
+++ week11-lab-tasks/src/main/java/day04/Service.java
@@ -0,0 +1,11 @@
+package day04;
+
+import java.time.LocalDate;
+
+public class Service extends Item{
+
+    public Service(String name, int price) {
+        super(name, price);
+        super.setBestBefore(LocalDate.now().plusYears(1));
+    }
+}
